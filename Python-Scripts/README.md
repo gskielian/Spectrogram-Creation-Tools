398432d2ccfed21968128b6476546ee3f89d4193
gskielian <gregory.kielian@gmail.com>
2016-07-17 18:08:42 -0700

    add python spectrogram script and readme

---
+++ Python-Scripts/README.md
@@ -0,0 +1,22 @@
+# Python Spectrogram
+
+create spectrograms with python : )
+
+##Usage
+
+```sh
+python wav-to-spectrogram.py testing123.wav
+```
+
+this will create a spectrogram file, e.g.:
+
+`spectrogram_-_testing123.png`
+
+## Workflow
+
+Use Audacity and export as microsoft 16 bit wav (python's wave library can only work with the 16 bit wavs right now)
+
+## Dependencies:
+
+- libpng
+- matplotlib
---
+++ Python-Scripts/wav-to-spectrogram.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import wave
+import pylab
+import sys
+
+def create_spectrogram(wav_file):
+    sound_info, frame_rate = get_wav_data(wav_file)
+    pylab.figure(num=None, figsize=(20, 10))
+    pylab.axis('off')
+    pylab.specgram(sound_info, Fs=frame_rate)
+    pylab.savefig('spectrogram_-_%s' % os.path.splitext(wav_file)[0],bbox_inches='tight', pad_inches = 0)
+
+
+def get_wav_data(wav_file):
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pylab.fromstring(frames, 'Int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    return sound_info, frame_rate
+
+if __name__ == '__main__':
+    wav_file = sys.argv[1]
+    create_spectrogram(wav_file)
